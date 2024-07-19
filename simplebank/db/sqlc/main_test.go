4ba7c00f6f2e1d8b87838327ae671cc534183215
Anton Golosnichenko <a.golosnichenko@icloud.com>
2024-07-20 00:09:59 +0500

    feat: config management

--- simplebank/db/sqlc/main_test.go
+++ simplebank/db/sqlc/main_test.go
@@ -7,11 +7,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/agolosnichenko/simplebank/util"
 	"github.com/jackc/pgx/v5/pgxpool"
-)
-
-const (
-	dbSource = "postgresql://golang:golang@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
@@ -20,19 +17,24 @@ var testDB *pgxpool.Pool
 func TestMain(m *testing.M) {
 	var err error
 
-	config, err := pgxpool.ParseConfig(dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatalf("cannot load config: %v", err)
+	}
+
+	dbConfig, err := pgxpool.ParseConfig(config.DbSource)
 	if err != nil {
 		log.Fatalf("Unable to parse connection string: %v", err)
 	}
 
 	// Configure connection pool settings
-	config.MaxConns = 10                   // Set the maximum number of connections
-	config.MinConns = 1                    // Set the minimum number of connections
-	config.MaxConnLifetime = 0             // No maximum connection lifetime
-	config.MaxConnIdleTime = 0             // No maximum idle time
-	config.HealthCheckPeriod = time.Minute // Set the health check period
+	dbConfig.MaxConns = 10                   // Set the maximum number of connections
+	dbConfig.MinConns = 1                    // Set the minimum number of connections
+	dbConfig.MaxConnLifetime = 0             // No maximum connection lifetime
+	dbConfig.MaxConnIdleTime = 0             // No maximum idle time
+	dbConfig.HealthCheckPeriod = time.Minute // Set the health check period
 
-	testDB, err = pgxpool.NewWithConfig(context.Background(), config)
+	testDB, err = pgxpool.NewWithConfig(context.Background(), dbConfig)
 	if err != nil {
 		log.Fatalf("Unable to create connection pool: %v", err)
 	}
--- simplebank/main.go
+++ simplebank/main.go
@@ -7,28 +7,28 @@ import (
 
 	"github.com/agolosnichenko/simplebank/api"
 	db "github.com/agolosnichenko/simplebank/db/sqlc"
+	"github.com/agolosnichenko/simplebank/util"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-const (
-	dbSource      = "postgresql://golang:golang@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	config, err := pgxpool.ParseConfig(dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatalf("cannot load config: %v", err)
+	}
+	dbConfig, err := pgxpool.ParseConfig(config.DbSource)
 	if err != nil {
 		log.Fatalf("Unable to parse connection string: %v", err)
 	}
 
 	// Configure connection pool settings
-	config.MaxConns = 10                   // Set the maximum number of connections
-	config.MinConns = 1                    // Set the minimum number of connections
-	config.MaxConnLifetime = 0             // No maximum connection lifetime
-	config.MaxConnIdleTime = 0             // No maximum idle time
-	config.HealthCheckPeriod = time.Minute // Set the health check period
+	dbConfig.MaxConns = 10                   // Set the maximum number of connections
+	dbConfig.MinConns = 1                    // Set the minimum number of connections
+	dbConfig.MaxConnLifetime = 0             // No maximum connection lifetime
+	dbConfig.MaxConnIdleTime = 0             // No maximum idle time
+	dbConfig.HealthCheckPeriod = time.Minute // Set the health check period
 
-	conn, err := pgxpool.NewWithConfig(context.Background(), config)
+	conn, err := pgxpool.NewWithConfig(context.Background(), dbConfig)
 	if err != nil {
 		log.Fatalf("Unable to create connection pool: %v", err)
 	}
@@ -37,7 +37,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatalf("Cannot start server: %v", err)
