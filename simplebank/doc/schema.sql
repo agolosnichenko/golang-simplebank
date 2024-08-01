f74869f6e1f5f5d4122bc9b97284724f53a05d19
Anton Golosnichenko <a.golosnichenko@icloud.com>
2024-08-02 02:13:50 +0500

    feat: dbdocks

---
+++ simplebank/doc/schema.sql
@@ -0,0 +1,74 @@
+-- SQL dump generated using DBML (dbml.dbdiagram.io)
+-- Database: PostgreSQL
+-- Generated at: 2024-08-01T21:10:24.211Z
+
+CREATE TABLE "accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" text NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" text NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+  "id" bigint PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+  "id" bigint PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "users" (
+  "username" text PRIMARY KEY,
+  "hashed_password" text NOT NULL,
+  "full_name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01',
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "sessions" (
+  "id" uuid PRIMARY KEY,
+  "username" string NOT NULL,
+  "refresh_token" text NOT NULL,
+  "user_agent" text NOT NULL,
+  "client_ip" text NOT NULL,
+  "is_blocked" boolean NOT NULL DEFAULT false,
+  "expires_at" timestamptz NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE UNIQUE INDEX "owner_currency_key" ON "accounts" ("owner", "currency");
+
+CREATE INDEX "accounts_owner_idx" ON "accounts" ("owner");
+
+CREATE INDEX "entries_account_id_idx" ON "entries" ("account_id");
+
+CREATE INDEX "transfers_from_account_id_idx" ON "transfers" ("from_account_id");
+
+CREATE INDEX "transfers_to_account_id_idx" ON "transfers" ("to_account_id");
+
+CREATE INDEX "transfers_from_account_id_to_account_id_idx" ON "transfers" ("from_account_id", "to_account_id");
+
+CREATE INDEX "users_email_key" ON "users" ("email");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
+
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
+
+ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "sessions" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
